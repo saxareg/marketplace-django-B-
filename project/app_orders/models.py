109e75661f5745628c0621caa1330ea8c7f90e6c
BogdanMalashuk <kufar.120@gmail.com>
2025-04-16 00:14:30 +0300

    debugged imports

--- project/app_orders/models.py
+++ project/app_orders/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ..app_products.models import Product
-from ..app_users.models import PickupPoints
-from ..app_shops.models import Shop
+from app_products.models import Product
+from app_users.models import PickupPoints
+from app_shops.models import Shop
 # Create your models here.
 
 """
