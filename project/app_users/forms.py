11903f685838ecf8a356b22fb96cfdc9235f8cf9
BogdanMalashuk <kufar.120@gmail.com>
2025-04-23 20:05:00 +0300

    self profile editing forms (phone, email, username) was added

--- project/app_users/forms.py
+++ project/app_users/forms.py
@@ -4,10 +4,41 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
 
-class UserProfileForm(forms.ModelForm):
+class PhoneForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['phone', 'role', 'pickup_point']
+        fields = ['phone']
+
+
+class UserProfileUpdateForm(forms.ModelForm):
+    username = forms.CharField(label='Username', max_length=150)
+    email = forms.EmailField(label='Email')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exclude(id=self.instance.id).exists():
+            raise forms.ValidationError("This nickname is already taken")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exclude(id=self.instance.id).exists():
+            raise forms.ValidationError("This email is already taken")
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.username = self.cleaned_data['username']
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        user_profile = user.profile
+        user_profile.save()
+        return user
 
 
 class PickupPointForm(forms.ModelForm):
@@ -23,16 +54,8 @@ class UserLoginForm(AuthenticationForm):
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True, help_text='')
-    password1 = forms.CharField(
-        label="Пароль",
-        widget=forms.PasswordInput,
-        help_text=''
-    )
-    password2 = forms.CharField(
-        label="Подтверждение пароля",
-        widget=forms.PasswordInput,
-        help_text=''
-    )
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput, help_text='')
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput, help_text='')
 
     class Meta:
         model = User
@@ -44,5 +67,5 @@ class UserRegisterForm(UserCreationForm):
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Email уже используется.")
+            raise forms.ValidationError("This email is already taken")
         return email
--- project/app_users/views.py
+++ project/app_users/views.py
@@ -5,14 +5,15 @@ from django.contrib import messages
 from .forms import *
 
 
-# Представление для регистрации
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
+            profile = UserProfile(user=user)
+            profile.save()
             login(request, user)
-            return redirect('products')  # Перенаправление на главную страницу
+            return redirect('products')
         else:
             messages.error(request, "Ошибка при регистрации.")
     else:
@@ -45,4 +46,32 @@ def logout_view(request):
 
 @login_required
 def profile_view(request):
-    return render(request, 'users/profile.html', {'user': request.user})
+    user = request.user
+    user_profile = user.profile
+
+    user_form = UserProfileUpdateForm(instance=user)
+    phone_form = PhoneForm(instance=user_profile)
+
+    if request.method == 'POST':
+        form_type = request.POST.get('form_type')
+
+        if form_type == 'user_form':
+            user_form = UserProfileUpdateForm(request.POST, instance=user)
+            if user_form.is_valid():
+                user_form.save()
+                messages.success(request, 'Данные обновлены.')
+                return redirect('me')
+
+        elif form_type == 'phone_form':
+            phone_form = PhoneForm(request.POST, instance=user_profile)
+            if phone_form.is_valid():
+                phone_form.save()
+                messages.success(request, 'Номер телефона обновлен.')
+                return redirect('me')
+
+    return render(request, 'users/profile.html', {
+        'user_form': user_form,
+        'phone_form': phone_form,
+        'user_profile': user_profile
+    })
+
