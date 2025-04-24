aa4a98ec3ce2086c407bf6b4305d3d41f51aabec
BogdanMalashuk <kufar.120@gmail.com>
2025-04-24 14:37:13 +0300

    in model Shop default attribute value (logo) was added

--- project/app_shops/models.py
+++ project/app_shops/models.py
@@ -7,7 +7,7 @@ class Shop(models.Model):
     slug = models.SlugField(max_length=100, unique=True)  # SEO-дружественный URL
     description = models.TextField(blank=True, null=True)  # Описание магазина
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shops')  # Владелец
-    logo = models.ImageField(upload_to='shops/logos/', blank=True, null=True)  # Логотип
+    logo = models.ImageField(upload_to='shops/logos/', default='shops/logos/default.jpg', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)  # Время создания
     is_active = models.BooleanField(default=True, db_index=True)  # Активность магазина
     def __str__(self): return self.name
