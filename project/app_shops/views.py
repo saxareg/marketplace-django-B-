ce857450a9916b068cf76b2aca38b7258f70bc09
BogdanMalashuk <kufar.120@gmail.com>
2025-04-25 15:08:50 +0300

    developed shop create request function, removed unresolved imports

--- project/app_shops/views.py
+++ project/app_shops/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Shop
+from django.contrib.auth.decorators import login_required
+from .forms import ShopCreationRequestForm
+import os
+from django.contrib import messages
 
 
 def my_shops_view(request):
@@ -14,3 +18,37 @@ def my_shop_detail(request, slug):
         'shop': shop,
         'products': products
     })
+
+
+def shop_create_request(request):
+    if request.method == 'POST':
+        form = ShopCreationRequestForm(request.POST, request.FILES)
+        if form.is_valid():
+            shop_request = form.save(commit=False)
+            shop_request.user = request.user
+            shop_request.save()
+            return redirect('my-shops')
+    else:
+        form = ShopCreationRequestForm()
+
+    return render(request, 'shops/shop_create_request.html', {'form': form})
+
+
+@login_required
+def shop_delete(request, slug):
+    shop = get_object_or_404(Shop, slug=slug)
+
+    if shop.owner != request.user:
+        return redirect('my-shops')
+
+    if request.method == 'POST':
+        if shop.logo:
+            logo_path = shop.logo.path
+            if os.path.isfile(logo_path):
+                os.remove(logo_path)
+
+        shop.delete()
+        return redirect('my-shops')
+
+    shops = Shop.objects.all()
+    return render(request, 'shops/shops.html', {'shops': shops})
