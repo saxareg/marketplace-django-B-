0566f7666656ed725e773e2deecaaaac8ebc749b
BogdanMalashuk <kufar.120@gmail.com>
2025-04-25 15:06:38 +0300

    added create and delete urls

--- project/app_shops/urls.py
+++ project/app_shops/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('my-shops/', views.my_shops_view, name='my-shops'),
     path('my-shops/<slug:slug>/', views.my_shop_detail, name='shop-detail'),
+    path('create/', views.shop_create_request, name='shop-create-request'),
+    path('delete/<slug:slug>/', views.shop_delete, name='shop-delete'),
 ]
