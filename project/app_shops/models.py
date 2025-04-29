55339f01eb8da9e539e8f0582ab6388951babea9
BogdanMalashuk <kufar.120@gmail.com>
2025-04-29 14:29:43 +0300

    remove field owner

--- project/app_shops/models.py
+++ project/app_shops/models.py
@@ -7,7 +7,6 @@ class BaseShop(models.Model):
     name = models.CharField(max_length=100, unique=True)  # Название магазина
     slug = models.SlugField(max_length=100, unique=True)  # SEO-дружественный URL
     description = models.TextField(blank=True, null=True)  # Описание магазина
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shops', null=True, blank=True)  # Владелец
     logo = models.ImageField(upload_to='shops/logos/', default='shops/logos/default.jpg', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)  # Время создания
     is_active = models.BooleanField(default=True, db_index=True)  # Активность магазина
@@ -33,7 +32,6 @@ class ShopCreationRequest(BaseShop):
                                        ('approved', 'Одобрена'),
                                        ('rejected', 'Отклонена')], default='pending')
     response_time = models.DateTimeField(null=True, blank=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='requests')
 
     def __str__(self):
         return f"Заявка на создание магазина: {self.name} от {self.user.username}"
