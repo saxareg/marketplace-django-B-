916ce0b2a51ccf0172a9869452c099aca6d7d755
BogdanMalashuk <kufar.120@gmail.com>
2025-05-04 17:50:03 +0300

    urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,11 +1,10 @@
-from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('custom-admin/', include('app_admin.urls')),
     path('products/', include('app_products.urls')),
     path('users/', include('app_users.urls')),
     path('cart/', include('app_orders.urls')),
