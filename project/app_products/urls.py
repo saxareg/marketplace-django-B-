b8da837fd0a1269388eeb94dbe6fa532509fe019
BogdanMalashuk <kufar.120@gmail.com>
2025-05-05 13:21:23 +0300

    add review url was added

--- project/app_products/urls.py
+++ project/app_products/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('<slug:slug>/', views.product_detail, name='product_detail'),
     path('<slug:slug>/delete/', views.delete_product, name='delete_product'),
     path('<slug:slug>/edit/', views.edit_product, name='edit_product'),
-
+    path('<slug:slug>/add-review/', views.add_review, name='add-review'),
 ]
