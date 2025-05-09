00f430aa4f0cf0fe9d626b6b847d037724f4185d
vldbel <vldbel@gmail.com>
2025-05-09 18:21:44 +0000

    swithcing to .env file with local private project settings - fix

--- project/project/settings.py
+++ project/project/settings.py
@@ -160,6 +160,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
