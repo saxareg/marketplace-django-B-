cce7f68b4accbcc29e5e86ce23e05f15d18036a8
BogdanMalashuk <kufar.120@gmail.com>
2025-05-11 23:45:56 +0300

    context processor with cart items counter was added

--- project/app_users/utils/context_processors.py
+++ project/app_users/utils/context_processors.py
@@ -1,4 +1,15 @@
+from app_orders.models import CartItem
+
+
 def user_profile(request):
     if request.user.is_authenticated:
         return {'userprofile': request.user.profile}
     return {'userprofile': None}
+
+
+def cart_item_count(request):
+    count = 0
+    if request.user.is_authenticated:
+        cart_items = CartItem.objects.filter(cart__user=request.user)
+        count = sum(item.quantity for item in cart_items)
+    return {'cart_item_count': count}
