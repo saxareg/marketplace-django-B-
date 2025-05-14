eaed86384b9c944e061c3f5852b1582559d1ad79
BogdanMalashuk <kufar.120@gmail.com>
2025-05-14 15:14:38 +0300

    PEP8, comments was added

--- project/app_orders/forms.py
+++ project/app_orders/forms.py
@@ -4,12 +4,22 @@ from app_users.models import PickupPoints
 
 
 class CartItemForm(forms.ModelForm):
+    """
+    Form for editing a cart item.
+    Allows the user to select a product and specify its quantity.
+    """
+
     class Meta:
         model = CartItem
         fields = ['product', 'quantity']
 
 
 class OrderStatusUpdateForm(forms.ModelForm):
+    """
+    Admin form for updating the status of an order.
+    Only allows specific status choices: Ready for pickup, Delivered, or Returned.
+    """
+
     class Meta:
         model = Order
         fields = ['status']
@@ -18,18 +28,28 @@ class OrderStatusUpdateForm(forms.ModelForm):
         }
 
     def __init__(self, *args, **kwargs):
+        """
+        Initializes the form with a restricted list of allowed order statuses.
+        This prevents setting arbitrary statuses from the admin interface.
+        """
+
         super().__init__(*args, **kwargs)
         allowed_statuses = [
-            ('ready_for_pickup', 'Готов к выдаче'),
-            ('delivered', 'Доставлен'),
-            ('returned', 'Возвращён')
+            ('ready_for_pickup', 'Ready for pickup'),
+            ('delivered', 'Delivered'),
+            ('returned', 'Returned')
         ]
         self.fields['status'].choices = allowed_statuses
 
 
 class OrderCreate(forms.Form):
+    """
+    Form for placing an order.
+    Allows the customer to select a payment method and a pickup point.
+    """
+
     payment_method = forms.ChoiceField(
-        choices=[('online', 'Онлайн'), ('offline', 'На месте')],
+        choices=[('online', 'Online'), ('offline', 'Offline')],
         widget=forms.Select(attrs={'class': 'form-control'}),
         required=True,
         error_messages={'required': ''},
