6bca2c3e0f5b78cc40e49a88626872546948af26
BogdanMalashuk <kufar.120@gmail.com>
2025-05-14 13:25:28 +0300

    PEP8

--- project/project/celery.py
+++ project/project/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
@@ -8,15 +9,15 @@ app = Celery('project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
+
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
 
-from celery.schedules import crontab
 
 CELERY_BEAT_SCHEDULE = {
     'mark_unclaimed_orders_every_day': {
         'task': 'app_orders.tasks.mark_unclaimed_orders',
-        'schedule': crontab(minute=0, hour=0),  # каждый день в 00:00
+        'schedule': crontab(minute=0, hour=0),
     },
 }
