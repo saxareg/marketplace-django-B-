e35076f4e95da110f4405754451bd573e939c557
BogdanMalashuk <kufar.120@gmail.com>
2025-05-19 20:24:21 +0300

    toggle shop status (to AJAX request) view was added

--- project/app_shops/views.py
+++ project/app_shops/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Shop
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from .forms import ShopCreationRequestForm
 import os
+import json
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 
 def my_shops_view(request):
@@ -17,6 +21,23 @@ def my_shop_detail(request, slug):
         'shop': shop,
         'products': products
     })
+
+
+@require_POST
+@login_required
+def toggle_shop_status(request, slug):
+    if not request.user.is_authenticated:
+        return JsonResponse({'error': 'Unauthorized'}, status=401)
+    try:
+        data = json.loads(request.body)
+        shop = Shop.objects.get(slug=slug, owner=request.user)
+        shop.is_active = data['is_active']
+        shop.save()
+        return JsonResponse({'success': True, 'is_active': shop.is_active})
+    except Shop.DoesNotExist:
+        return JsonResponse({'error': 'Shop not found'}, status=404)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
 
 
 def shop_create_request(request):
