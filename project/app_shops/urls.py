532c5ce3d483812467090d690b870c6aff9f98bb
BogdanMalashuk <kufar.120@gmail.com>
2025-05-19 20:22:04 +0300

    toggle shop status url was added

--- project/app_shops/urls.py
+++ project/app_shops/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('my-shops/<slug:slug>/', views.my_shop_detail, name='shop-detail'),
     path('create/', views.shop_create_request, name='shop-create-request'),
     path('delete/<slug:slug>/', views.shop_delete, name='shop-delete'),
+    path('my-shops/<slug:slug>/toggle-status/', views.toggle_shop_status, name='toggle_shop_status'),
 ]
